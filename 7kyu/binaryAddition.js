d326bae08fe6567e32ffa4b51fc531160fa9cbec
Laurel Younis <laurelyounisdev@gmail.com>
2016-10-10 06:31:11 -0700

    daily coding challenge

--- 7kyu/binaryAddition.js
+++ 7kyu/binaryAddition.js
@@ -12,4 +12,28 @@ Examples:(Input1, Input2 --> Output (explanation)))
 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
+//input / params are going to be 2 nums 2 inputs
+// always whole positive nums
+//return the 2 params added together which is their sum, the sum is converted into binary needs to be returned as a string 
+//start out by adding the inputs togther to get the sum, 
+//take the sum convert it to binary 
+//convert the binary sum to a string
+//return the string
+
+
+
+
+
 */
+
+function addBinary(n1, n2){
+ let sum= n1 + n2 
+ //convert the sum to binary
+
+ //return this as a string
+ return 
+}
+
+
+console.log(addBinary(1, 1)) //2 => 10
+console.log(addBinary(5, 9 )) //14 => 1110
