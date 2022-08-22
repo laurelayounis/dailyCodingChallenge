25b38dd10b51f3186b221b09aa2e6350c329f32e
Laurel Younis <laurelyounisdev@gmail.com>
2022-08-22 15:51:41 -0700

    daily coding challenge

--- 8kyu/keepUptheHoop.js
+++ 8kyu/keepUptheHoop.js
@@ -21,4 +21,10 @@ function hoopCount (n) {
    }
  }
 
+ //refactored with terinary
+
+ function hoopCount (n) {
+    return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+  }
+
  
