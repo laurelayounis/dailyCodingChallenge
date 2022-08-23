8e84d73657aabd3a282b1b94aa2e9dd2be9c4f10
Laurel Younis <laurelyounisdev@gmail.com>
2022-08-23 15:50:46 -0700

    daily coding challenge

--- 8kyu/alternatingCase.js
+++ 8kyu/alternatingCase.js
@@ -12,3 +12,7 @@ selected language; see the initial solution for details) such that each lowercas
 
 */
 
+String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+ }
+
