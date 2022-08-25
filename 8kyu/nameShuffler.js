b8114714b6915712dbc7036c34c8726140919465
Laurel Younis <laurelyounisdev@gmail.com>
2022-08-24 18:49:43 -0700

    daily coding challenge

---
+++ 8kyu/nameShuffler.js
@@ -0,0 +1,18 @@
+/*
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+*/
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+   }
+
+
+   //alt solution
+
+   const nameShuffler = str => str.split(' ').reverse().join(' ')
