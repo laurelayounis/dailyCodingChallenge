0cc03e8713fbe822fb1609c20afc608ad75e34bb
Laurel Younis <laurelyounisdev@gmail.com>
2022-08-29 12:52:22 -0700

    daily coding challenge

--- 8kyu/cutIt.js
+++ 8kyu/cutIt.js
@@ -23,3 +23,4 @@ function cutIt(arr){
     let min = Math.min(...arr.map(x => x.length))
     return arr.map(x => x.slice(0, min))
     
+    ..
--- 8kyu/simpleComparison.js
+++ 8kyu/simpleComparison.js
@@ -15,3 +15,6 @@ function add(a, b){
   function add(a, b){
     return (a == b ? true: false)
   }
+
+
+  
