ed2a16796eb2692b599f367329f6da4b62918b88
Laurel Younis <laurelyounisdev@gmail.com>
2022-08-31 13:25:02 -0700

    daily coding challenge

---
+++ 8kyu/sumTheStrings.js
@@ -0,0 +1,17 @@
+/*
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+Notes:
+
+If either input is an empty string, consider it as zero.
+
+*/
+
+function sumStr(a,b) {
+    return String(+a + +b)
+    
+  }
