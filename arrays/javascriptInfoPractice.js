0729b9e59be42a7ed38c44fdb4f4f671dacbbf06
Laurel Younis <laurelyounisdev@gmail.com>
2022-09-04 09:37:42 -0700

    daily coding challenge

--- arrays/javascriptInfoPractice.js
+++ arrays/javascriptInfoPractice.js
@@ -46,4 +46,19 @@ alert( filtered ); // 3,1 (matching values)
 
 alert( arr ); // 5,3,8,1 (not modified)
 
+function filterRangeInPlace(arr, a, b) {
 
+    for (let i = 0; i < arr.length; i++) {
+      let val = arr[i];
+  
+      // remove if outside of the interval
+      if (val < a || val > b) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+  
+  }
+  
+  
+  
