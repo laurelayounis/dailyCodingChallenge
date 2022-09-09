881111689ae4f3b6022876d6a874d6370f7c0881
Laurel Younis <laurelyounisdev@gmail.com>
2022-09-09 13:57:34 -0700

    daily coding challenge

--- arrays/javascriptInfoPractice.js
+++ arrays/javascriptInfoPractice.js
@@ -61,4 +61,13 @@ function filterRangeInPlace(arr, a, b) {
   }
   
   
-  
+  /*
+  Sort in decreasing order
+importance: 4
+let arr = [5, 2, 1, -10, 8];
+
+// ... your code to sort it in decreasing order
+
+alert( arr ); // 8, 5, 2, 1, -10
+*/
+
