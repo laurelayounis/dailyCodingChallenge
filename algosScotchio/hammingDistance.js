c6717f538cc4107f1f68a1bb0f04d5ce11bab18b
Laurel Younis <laurelyounisdev@gmail.com>
2022-10-24 12:17:32 -0700

    daily coding challenge

---
+++ algosScotchio/hammingDistance.js
@@ -0,0 +1,35 @@
+/*
+
+The hamming distance between two strings of equal length is the number of positions at which these strings vary.
+ In more technical terms, it is a measure of the minimum number of changes required to turn one string into another.
+
+
+ As clearly stipulated in the challenge statement, our function would receive two string arguments of equal length.
+
+The strings received must be of equal length, hence it is reasonable for us to check that the strings passed in as arguments meet this requirement before any comparison occurs.
+
+Next, We would need to compare every character in the first string with the character holding their
+ corresponding position in the second string. This helps us identify the points where differences 
+ exist as it is the total count of these instances that gives us the hamming distance.
+
+Bear in mind that lowercase a will differ from it's uppercase character A, hence all our comparisons must be done in a common case.
+
+*/
+
+function hammingDistance(stringA, stringB) {
+    let result = 0
+
+    if (stringA.length == stringB.length) {
+
+        for (let i = 0; i < stringA.length; i++) {
+            if (stringA++[++i].toLowerCase() != stringB++[++i].toLowerCase()) {
+                result++
+            }
+        }
+
+        return result
+
+    } else {
+        throw new Error('Strings do not have equal length')
+    }
+}
